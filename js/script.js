b853ee7e29659632dd1cdb336c02015cee0a06e7
ma66ot <ma66ot@ma66ot-PC>
2014-07-14 17:08:38 +0200

    First commit to be safe that i dont loose this what i did till now

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function() {
+  $("button").on("click", function() {
+    $(".prod-icon").addClass("suggested");
+  });
+
+  $(document).ready(function() {
+    $(".tooltip").tooltip();
+  });
+  
+  PointerEventsPolyfill.initialize({});
+});
